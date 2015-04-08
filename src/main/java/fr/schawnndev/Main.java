93be5619addd84bfe1f76a1e4dfd49d337478977
SchawnnDev <iperskill@gmail.com>
2015-04-08 20:35:24 +0200

    Added commands + change Game

--- src/main/java/fr/schawnndev/Main.java
+++ src/main/java/fr/schawnndev/Main.java
@@ -3,20 +3,31 @@ package fr.schawnndev;
 import fr.schawnndev.files.FileManager;
 import fr.schawnndev.game.Game;
 import fr.schawnndev.game.GameManager;
+import fr.schawnndev.utils.LocationSerializer;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by SchawnnDev on 04/04/2015.
  *  <b>Infos</b>
- *  <ul>Lombok is a add-on (plugin) to simplify the code "esthetic"</ul>
+ *  <ul>Lombok is a add-on (plugin) to simplify the code and and to improve the aesthetics "esthetic"</ul>
  *  <ul>Lombok disappears during the compilation (build)</ul>
+ *  <ul>The plugin is normaly a <b>multi-game plugin</b>, but i didn't have time to finish..</ul>
+ *  <ul>You can add games with create a <b>new</b> Game (example in commands)</ul>
+ *  <ul>Why english ? But i'm french (:</ul>
  */
 public class Main extends JavaPlugin {
 
@@ -49,6 +60,28 @@ public class Main extends JavaPlugin {
 
         GameManager.initWeapons();
 
+        // > Game < //
+
+        Location playerSpawn = LocationSerializer.stringToLocation((String) FileManager.getCoordsConfig().get("playerSpawn"), true);
+
+        Location middleBirdsSpawn =  LocationSerializer.stringToLocation((String) FileManager.getCoordsConfig().get("birdsSpawn"), true);
+
+        List<Location> birdsLocations = new ArrayList<>();
+
+        birdsLocations.add(middleBirdsSpawn);
+
+        for(int i = 0; i < 10; i+=2){
+            birdsLocations.add(middleBirdsSpawn.clone().add(0.0, 0.0, i));
+            birdsLocations.add(middleBirdsSpawn.clone().add(0.0, 0.0, -i));
+        }
+
+
+        defaultGame = new Game(1, 5, 35, playerSpawn, birdsLocations, 5);
+
+        // > Commands < //
+
+        getCommand("poulet").setExecutor(this);
+
 
     }
 
@@ -56,8 +89,107 @@ public class Main extends JavaPlugin {
 
     }
 
-    public static void main(String[] args){
-        System.out.println(new Random().nextLong() * 1000);
-    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        if(cmd.getName().equalsIgnoreCase("poulet")){
+
+            if(!(sender instanceof Player)){
+                sender.sendMessage("§cThe command sender must be a player !");
+                return true;
+            }
+
+            Player player = (Player)sender;
+
+            if(args.length == 0){
+
+                if(player.hasPermission("poulet.start") || player.isOp())
+                    player.sendMessage("§6Start a game: §7/poulet start");
+
+                if(player.hasPermission("poulet.stop") || player.isOp())
+                    player.sendMessage("§6Stopping a game: §7/poulet stop");
+
+                player.sendMessage("§6Joining a game: §7/poulet join");
+                player.sendMessage("§6Quit a game: §7/poulet quit");
+
+                return true;
+            } else if (args.length == 1){
+                if(args[0].equalsIgnoreCase("start")){
+
+                    if(player.hasPermission("poulet.start") || player.isOp()) {
+
+                        if (getDefaultGame().isCurrentlyRunning()) {
+                            player.sendMessage("§cThe game has already started !");
+                            player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+                            return true;
+                        } else {
+                            player.sendMessage("§aStarting game....");
+                            player.playSound(player.getLocation(), Sound.VILLAGER_YES, 1.0F, 1.0F);
+                            getDefaultGame().start();
+                            return true;
+                        }
+
+                    } else {
+                        player.sendMessage("§cYou don't have permission to use this command !");
+                        player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+                        return true;
+                    }
+
+                } else if(args[0].equalsIgnoreCase("stop")){
+
+                    if(player.hasPermission("poulet.stop") || player.isOp()) {
+
+                        if (getDefaultGame().isCurrentlyRunning()) {
+                            player.sendMessage("§cThe game isn't running !");
+                            player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+                            return true;
+                        } else {
+                            player.sendMessage("§aStopping game....");
+                            getDefaultGame().stop();
+                            player.playSound(player.getLocation(), Sound.VILLAGER_YES, 1.0F, 1.0F);
+                            return true;
+                        }
+
+                    } else {
+                        player.sendMessage("§cYou don't have permission to use this command !");
+                        player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+                        return true;
+                    }
+
+                } else if(args[0].equalsIgnoreCase("join")){
+
+                    if (getDefaultGame().isCurrentlyRunning()) {
+                        player.sendMessage("§cThe game isn't running !");
+                        player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+                        return true;
+                    } else {
+                        player.sendMessage("§aJoining game....");
+                        getDefaultGame().addPlayer(player.getUniqueId());
+                        player.playSound(player.getLocation(), Sound.VILLAGER_YES, 1.0F, 1.0F);
+                        return true;
+                    }
+
+                } else if(args[0].equalsIgnoreCase("quit")){
+
+                    if (getDefaultGame().isCurrentlyRunning()) {
+                        player.sendMessage("§cThe game isn't running !");
+                        player.playSound(player.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+                        return true;
+                    } else {
+                        getDefaultGame().removePlayer(player.getUniqueId());
+                        player.sendMessage("§aYou left the game !");
+                        player.playSound(player.getLocation(), Sound.VILLAGER_YES, 1.0F, 1.0F);
+                        return true;
+                    }
+
+                }
+            } else {
+                player.sendMessage("§cToo many args !");
+                return true;
+            }
+        }
+
+        return false;
+
+    }
 }
---
+++ src/main/java/fr/schawnndev/utils/LocationSerializer.java
@@ -0,0 +1,43 @@
+package fr.schawnndev.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+/**
+ * Created by Paul on 03/04/2015.
+ */
+public class LocationSerializer {
+
+    public static String locationToString(Location location, boolean pitchAndYaw){
+       String loc = null;
+
+       if(pitchAndYaw)
+           loc = "" + location.getWorld().toString() + " " + location.getX() + " " + location.getY() + " " + location.getZ() + " " + location.getYaw() + " " + location.getPitch();
+        else
+           loc = "" + location.getWorld().toString() + " " + location.getX() + " " + location.getY() + " " + location.getZ();
+
+        return loc;
+    }
+
+    public static Location stringToLocation(String location, boolean pitchAndYaw){
+        Location loc = null;
+        String[] splitted = location.split(" ");
+        World world = Bukkit.getWorld(splitted[0]);
+        double x = Double.parseDouble(splitted[1]),
+                y = Double.parseDouble(splitted[2]),
+                z = Double.parseDouble(splitted[3]);
+
+        if(pitchAndYaw){
+            float yaw = Float.parseFloat(splitted[4]),
+                  pitch = Float.parseFloat(splitted[5]);
+
+            loc = new Location(world, x , y, z, yaw, pitch);
+        } else {
+            loc = new Location(world, x, y ,z);
+        }
+
+        return loc;
+    }
+
+}
